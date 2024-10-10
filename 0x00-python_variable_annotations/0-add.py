5b2242a51834a60b53422243b39b2171873a4316
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-10 14:18:26 +0000

    Done

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Script for taking two floats and returning their
+sum.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Sums two floats
+
+    Args:
+        a: First number
+        b: Second number
+
+    Returns:
+        Sum of the two floats
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Concatenate two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings
+
+    Args:
+        str1: First string
+        str2: Second string
+
+    Returns:
+        Concatenated string
+    """
+    return str1 + str2
