f2ba8b831a7685125b2710fe006a54ceacfc38aa
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-16 10:56:50 +0000

    Documenting 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,14 +7,14 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """_summary_
+    """Tasked version of wait_n implementation
 
     Args:
-        n: _description_
-        max_delay: _description_
+        n: Number of wait_random instances
+        max_delay: Max delay for each wait_random instance
 
     Returns:
-        _description_
+        A lists of all delay instances in ascending order
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = list()
