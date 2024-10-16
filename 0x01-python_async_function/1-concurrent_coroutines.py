1fde35738a5499ede2bd83ae9ae38e9752f74197
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-16 10:46:07 +0000

    Type annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Executing multiple coroutines concurrently"""
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Executes multiple wait_random() instances
 
     Args:
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Task based implementation of wait_n"""
 import asyncio
+from typing import List
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def task_wait_n(n, max_delay):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """_summary_
 
     Args:
